74e6df2a57e55aace8e2db4db7441aa3dc89f820
foreverchoi <foreverchoi0706@gmail.com>
2023-01-31 16:18:19 +0900

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up to date with 'origin/master'. # # Changes to be committed: #	modified:   src/constants/routes.ts #	new file:   src/pages/Adress/index.tsx #	modified:   src/pages/Home/index.tsx #

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,5 +1,6 @@
 import Home from "@/pages/Home";
 import About from "@/pages/About";
+import Address from "@/pages/Adress";
 
 const ROUTES = [
     {
@@ -7,12 +8,19 @@ const ROUTES = [
         path: "/",
         pathname: "HOME",
         isShowGnb: true
-    }, {
+    },
+    {
         Component: About,
         path: "/about/:id",
         pathname: "About",
         isShowGnb: false
 
+    },
+    {
+        Component: Address,
+        path: "/adress",
+        pathname: "adress",
+        isShowGnb: false
     }
 ] as const;
 
